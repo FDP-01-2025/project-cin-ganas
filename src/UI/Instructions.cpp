66710b6597990d96629d1ecccb2e7c75cba2ee6c
Diego <d14ooolivares@gmail.com>
2025-07-06 18:31:59 -0600

    Fix: Fixed instructions menu

--- src/UI/Instructions.cpp
+++ src/UI/Instructions.cpp
@@ -1,54 +1,61 @@
 #include <iostream>
 #include <windows.h>
 #include <limits>
-#include "Instructions.h"
 
 using namespace std;
 
-// Espera a que el usuario presione ENTER
-void WaitForEnter() {
+void WaitForEnter()
+{
     cout << "\nPresiona ENTER para continuar...";
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cin.get();
 }
 
-// Muestra encabezado centrado con espacios
-void PrintCenteredAscii() {
+void PrintCenteredAscii()
+{
     cout << R"(
-               __                        __                                      __      __                               
-              /  |                      /  |                                    /  |    /  |                              
-              $$/  _______    _______  _$$ |_     ______   __    __   _______  _$$ |_   $$/   ______   _______    _______ 
-              /  |/       \  /       |/ $$   |   /      \ /  |  /  | /       |/ $$   |  /  | /      \ /       \  /       |
-              $$ |$$$$$$$  |/$$$$$$$/ $$$$$$/   /$$$$$$  |$$ |  $$ |/$$$$$$$/ $$$$$$/   $$ |/$$$$$$  |$$$$$$$  |/$$$$$$$/ 
-              $$ |$$ |  $$ |$$      \   $$ | __ $$ |  $$/ $$ |  $$ |$$ |        $$ | __ $$ |$$ |  $$ |$$ |  $$ |$$      \ 
-              $$ |$$ |  $$ | $$$$$$  |  $$ |/  |$$ |      $$ \\__$$ |$$ \\_____   $$ |/  |$$ |$$ \\__$$ |$$ |  $$ | $$$$$$  |
-              $$ |$$ |  $$ |/     $$/   $$  $$/ $$ |      $$    $$/ $$       |  $$  $$/ $$ |$$    $$/ $$ |  $$ |/     $$/ 
-              $$/ $$/   $$/ $$$$$$$/     $$$$/  $$/        $$$$$$/   $$$$$$$/    $$$$/  $$/  $$$$$$/  $$/   $$/ $$$$$$$/  
-    )" << endl;
+$$$$$$\                       $$\                                             $$\                                         
+\_$$  _|                      $$ |                                            \__|                                        
+  $$ |  $$$$$$$\   $$$$$$$\ $$$$$$\    $$$$$$\  $$\   $$\  $$$$$$$\  $$$$$$$\ $$\  $$$$$$\  $$$$$$$\   $$$$$$\   $$$$$$$\ 
+  $$ |  $$  __$$\ $$  _____|\_$$  _|  $$  __$$\ $$ |  $$ |$$  _____|$$  _____|$$ |$$  __$$\ $$  __$$\ $$  __$$\ $$  _____|
+  $$ |  $$ |  $$ |\$$$$$$\    $$ |    $$ |  \__|$$ |  $$ |$$ /      $$ /      $$ |$$ /  $$ |$$ |  $$ |$$$$$$$$ |\$$$$$$\  
+  $$ |  $$ |  $$ | \____$$\   $$ |$$\ $$ |      $$ |  $$ |$$ |      $$ |      $$ |$$ |  $$ |$$ |  $$ |$$   ____| \____$$\ 
+$$$$$$\ $$ |  $$ |$$$$$$$  |  \$$$$  |$$ |      \$$$$$$  |\$$$$$$$\ \$$$$$$$\ $$ |\$$$$$$  |$$ |  $$ |\$$$$$$$\ $$$$$$$  |
+\______|\__|  \__|\_______/    \____/ \__|       \______/  \_______| \_______|\__| \______/ \__|  \__| \_______|\_______/                                                                                                                                                                                                                                                                                                                                                                           
+)" << endl;
 }
 
-// Muestra una instruccion
-void ShowInstruction(const string& texto, int numero, int total) {
+void ShowInstruction(const string &text, int num, int total)
+{
     system("cls");
     PrintCenteredAscii();
-    cout << "\nRegla " << numero << " de " << total << ":\n";
-    cout << texto << endl;
+    cout << "\nRegla " << num << " de " << total << ":\n";
+    cout << text << endl;
     WaitForEnter();
 }
 
-void ShowInstructions() {
-    cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Limpia buffer inicial
-
-    ShowInstruction("Instruccion 1: En el menu ingresar una opcion 1. para jugar, 2. para ver instrucciones, 3.para ver los creditos .", 1, 6);
-    ShowInstruction("Instruccion 2: Al presionar 1, aparecera un buho , el cual dara diversas opciones , como son: Jugar , Dormir , Comer, Ver estadisticas y salir , cada una tiene su respectivo numero para seleccionarlo. .", 2, 6);
-    ShowInstruction("Instruccion 3: Al escoger la opcion de jugar , nos apareceran diversas opciones de minijuegos en los cuales podremos ganar monedas.", 3, 6);
-    ShowInstruction("Instruccion 4: Al escoger la opcion de comer , aparecera un catalogo con las diversas comida que se le pueden dar a la mascota , con el incremento que ofrece y el precio..", 4, 6);
-    ShowInstruction("Instruccion 5: Al escoger la opcion de estadisticas , podremos ver las estdisticas de nuestra mascota : Tipo , Nombre , Felicidad , Energia , Hambre , Monedas.", 5, 6);
-    ShowInstruction("Instruccion 6: Al escoger la opcion de dormir... .", 5, 6);
-    
+void ShowInstructions()
+{
+    ShowInstruction("Instruccion 1: En el menu, ingresa una opcion: 1 para jugar, 2 para ver instrucciones, 3 para ver los creditos.", 1, 6);
+    ShowInstruction("Instruccion 2: Al presionar 1, aparecera un buho que dara diversas opciones: Jugar, Dormir, Comer, Ver estadisticas y Salir. Cada una tiene su respectivo numero para seleccionarla.", 2, 6);
+    ShowInstruction("Instruccion 3: Al escoger la opcion de Jugar, aparecera una lista de minijuegos en los cuales podras ganar monedas.", 3, 6);
+    ShowInstruction("Instruccion 4: Al escoger la opcion de Comer, aparecera un catalogo con las comidas disponibles, mostrando el incremento que ofrecen y su precio.", 4, 6);
+    ShowInstruction("Instruccion 5: En la opcion de Estadisticas podras ver los datos de tu mascota: Tipo, Nombre, Felicidad, Energia, Hambre y Monedas.", 5, 6);
+    ShowInstruction("Instruccion 6: Al escoger la opcion de dormir, tu mascota descansara y recuperara energia.", 6, 6);
 
     system("cls");
-    cout << "Fin de las instrucciones.\n";
-    cout << "Presiona ENTER para volver al menu...";
+    cout << "+------------------------------------------+\n";
+    cout << "|      Fin de las instrucciones.           |\n";
+    cout << "|   Presiona ENTER para volver al menu.    |\n";
+    cout << "+------------------------------------------+\n";
+
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
     cin.get();
+
+    Sleep(2000);
     system("cls");
+
+    return;
 }
+
